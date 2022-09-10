0b5c39d176e46babeca2ad2e5f03e951eed2c084
Thinh Duy <thinh.nd.60cntt@ntu.edu.vn>
2022-09-10 12:14:41 +0700

    design

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,69 @@
+export const Users = [
+    {
+      id:1,
+      profilePicture: "assets/person/thinh.jpg",
+      username: "Nguyen Duy Thinh",
+    },
+    {
+      id:2,
+      profilePicture: "assets/person/tai.jpg",
+      username: "Nguyen Tien Tai",
+    },
+    {
+      id:3,
+      profilePicture: "assets/person/thainam.jpg",
+      username: "Thai Van Nam",
+    },
+    {
+      id:4,
+      profilePicture: "assets/person/tri.jpg",
+      username: "Tran Ngoc Tri",
+    },
+    {
+      id:5,
+      profilePicture: "assets/person/vinh.jpg",
+      username: "Dang Hoang Vinh",
+    },
+    {
+      id:6,
+      profilePicture: "assets/person/thao.jpg",
+      username: "Nguyen Thuy Hanh Thao",
+    }
+  ];
+  
+  export const Posts = [
+    {
+      id: 1,
+      desc: "Love For All, Hatred For None.",
+      photo: "assets/post/post1.jpeg",
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: "assets/post/post2.jpeg",
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "Every moment is a fresh beginning.",
+      photo: "assets/post/post3.jpeg",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    {
+      id: 4,
+      photo: "assets/post/post1.jpeg",
+      date: "4 hours ago",
+      userId: 4,
+      like: 7,
+      comment: 3,
+    }
+  ];
